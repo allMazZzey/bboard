fbe2350612df064aff782ed449739c8aac09760e
allmazzzey <allmazzzey@gmail.com>
2023-11-13 05:17:04 +0300

    Added authorization

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -5,6 +5,7 @@
 @section('main')
 <h2>{{$ad->title}}</h2>
 <p>{{$ad->content}}</p>
-    <p>{{$ad->price}} руб.</p>
+<p>{{$ad->price}} руб.</p>
 <p>Автор: {{$ad->user->name}}</p>
-<p><a href="{{route('index')}}">Ко всем объявлениям</a></p> @endsection('main')
+<p><a href="{{route('index')}}">Ко всем объявлениям</a></p>
+@endsection('main')
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [AdsController::class, 'index'])->name('index');
 Route::get('/{ad}', [AdsController::class, 'detail'])->name('detail');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
